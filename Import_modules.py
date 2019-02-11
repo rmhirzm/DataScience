c9afa8736bca7d140abe252e2dcfafb6b4b72c71
Raminta Zmuidinaite <zmuidinaite.r@gmail.com>
2019-02-11 12:23:50 +0000

    testing

--- Import_modules.py
+++ Import_modules.py
@@ -9,4 +9,6 @@ import matplotlib.pyplot as plt		 	# Useful for data visualization
 from collections import Counter, defaultdict 	# If we want to import several features   
 from __future__ import division			# Usefull to simplify code: Makes / to results in rational number and an // in an integer     
 
-my_regex = re.compile("[0-9]+", re.I)    	# example of code whem you want do use a particular function of a module     
+my_regex = re.compile("[0-9]+", re.I)    	# example of code whem you want do use a particular function of a module 
+
+# end of file
